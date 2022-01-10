1c9a352a2372f5c9772ac9c343983e952c70733a
Mirza Kir <46354349+mKir04@users.noreply.github.com>
2022-01-10 14:34:49 -0500

    href updates

--- schedule_login.php
+++ schedule_login.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
     <title>AprilScheduler</title>
 
-    <!-- CSSS -->
+    <!-- CSS  -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <script src="https://kit.fontawesome.com/c1a26a6b8a.js" crossorigin="anonymous"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
@@ -20,13 +20,13 @@
         });
 
         document.addEventListener('DOMContentLoaded', function () {
-            var elems = document.querySelectorAll('.timepicker');
-            var instances = M.Timepicker.init(elems, {
-                defaultTime: 'now', // Set default time: 'now', '1:30AM', '16:30'
-                twelveHour: true, // Use AM/PM or 24-hour format
-                autoClose: false, // automatic close timepicker
+                var elems = document.querySelectorAll('.timepicker');
+                var instances = M.Timepicker.init(elems, {
+                    defaultTime: 'now', // Set default time: 'now', '1:30AM', '16:30'
+                    twelveHour: true, // Use AM/PM or 24-hour format
+                    autoClose: false, // automatic close timepicker
+                });
             });
-        });
     </script>
 </head>
 
@@ -47,46 +47,34 @@
 </nav>
 <main>
     <div class="container myElement">
-        <div class="row">
-            <h2>Schedule as guest</h2>
-        </div>
         <form class="col s12">
+            <div class="row">
+                <div class="col">
+                     <h1 class="header header-font">Schedule an appointment</h1>
+                </div>
+            </div>
+               
             <div class="row">
                 <div class="input-field col s4">
                     <input type="text" class="datepicker no-autoinit" name="date" id="date" placeholder="Please enter the date of the appointment" required>
                     <label style="font-size: 1.3em" for="date">Date</label>
                 </div>
-
+            </div>
+            <div class="row">
                 <div class="input-field col s4">
                     <input type="text" class="timepicker no-autoinit" name="time" id="time" placeholder="Please enter the time of the appointment" required>
                     <label style="font-size: 1.3em" for="time">Time</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s4">
-                    <input type="text" class="validate" name="lastName" id="lastName" placeholder="Please enter your last name" required>
-                    <label style="font-size: 1.3em" for="lastName">Last Name</label>
+                <div class="col s4">
+                    <a class="btn-large waves-effect waves-light" href="payment_login.html">
+                        Schedule and Pay <i class="material-icons right">send</i>
+                    </a>
                 </div>
-                <div class="input-field col s4">
-                    <input type="text" class="validate" name="firstName" id="firstName" placeholder="Please enter your first name" required>
-                    <label style="font-size: 1.3em" for="lastName">First Name</label>
-                </div>
-                <text style="font-size: 2em; padding: 2em">Or</text>
-                <a href="login.html" id="download-button" class="btn-large waves-effect waves-light orange hoverable">Log In</a>
-            </div>
-            <div class="row">
-                <div class="input-field col s4">
-                    <input type="tel" class="validate" name="phone" id="phone" pattern="^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$" required>
-                    <label for="phone">Telephone</label>
-                </div>
-            </div>
-            <div class="row">
-                <a href="payment_guest.html" id="download-button" class="btn-large waves-effect waves-light hoverable">
-                    Schedule and Pay
-                    <i class="material-icons right">send</i></a>
             </div>
         </form>
-    </div>
+
 </main>
 <footer class="page-footer orange">
     <div class="container">
@@ -119,8 +107,8 @@
 
 <!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="../js/materialize.js"></script>
-<script src="../js/init.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
 
 
 </html>
