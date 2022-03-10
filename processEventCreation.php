3272a13334e3e6f8de543db2f79f2d136e377646
Mirza Kir <46354349+mKir04@users.noreply.github.com>
2022-03-10 08:16:25 -0500

    capacity on events + scheduling

---
+++ processEventCreation.php
@@ -0,0 +1,24 @@
+<?php
+$isVirtual = htmlentities($_REQUEST['isVirtual']);
+$scheduled_date = htmlentities($_REQUEST['scheduled_date']);
+$scheduled_time = htmlentities($_REQUEST['scheduled_time']);
+$address = htmlentities($_REQUEST['address']);
+$description = htmlentities($_REQUEST['description']);
+$capacity = htmlentities($_REQUEST['capacity']);
+$name = htmlentities($_REQUEST['name']);
+
+require_once('api.php');
+$api = new AprilInstituteScheduler_API();
+$api -> connect();
+
+$date_array = explode('-', $scheduled_date);
+$scheduled_date_time = $date_array[2] . '-' . $date_array[0] . '-' . $date_array[1] . 'T' . substr($scheduled_time, 0, 5);
+
+$result = $api -> createEvent($isVirtual, $scheduled_date_time, $address, $description, $capacity, $name);
+
+$api -> disconnect();
+
+if(!empty($result)) {
+    header("Location: home.php");
+    exit();
+}
