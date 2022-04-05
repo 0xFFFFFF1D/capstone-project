5da35adb781e0c7fe26e064bc72fc754e1f5ad44
Mirza Kir <46354349+mKir04@users.noreply.github.com>
2022-04-04 22:42:52 -0400

    updated events tables

--- home.php
+++ home.php
@@ -8,13 +8,21 @@ if(isset($_SESSION['uid'])) {
             <h1 class="header center april-orange-text header-font">Welcome, <?php echo $_SESSION['first_name'];?></h1>
             
             <div class="row center">
-                <?php include("getEventsTable.php");?>
+                <?php
+                    if($_SESSION['isAdmin']) include("getEventsTable_admin.php");
+                    else include("getEventsTable.php"); ?>
             </div>
             <div class="row center">
                 <a class="btn-floating btn-large waves-effect waves-light april-orange" href="schedule.php"><i class="material-icons">add</i></a>
             </div>
         </div>
     </div>
+    <script>
+        var elem = document.querySelector('.collapsible.expandable');
+        var instance = M.Collapsible.init(elem, {
+            accordion: false
+        });
+    </script>
 <?php
 include("template/base_footer.php");
 }
