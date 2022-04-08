0dc7f050e260e488a56905153c25cbfdb820d2e6
Ryan Kim <ryansci123@gmail.com>
2022-04-08 11:54:36 -0400

    Adds payment processing page and scheduling
    paymentPage.php is the landing page for any appointments
    made. After processing the payment through Helcim, paymentPage.php
    redirects back to home.php, where a Toast (from Materialize) appears,
    informing the user of their payment being processed.

--- home.php
+++ home.php
@@ -2,7 +2,11 @@
 session_start();
 
 if(isset($_SESSION['uid'])) {
-    $title="Home"; include("template/base_header.php");?>
+    $title="Home"; include("template/base_header.php");
+    if (isset($_REQUEST['paid'])) {
+        echo "<script>M.toast({html: 'You payment was processed successfully!'})</script>";
+    }
+    ?>
     <div class="container" id="index-banner">
         <div class="section col">
             <h1 class="header center april-orange-text header-font">Welcome, <?php echo $_SESSION['first_name'];?></h1>
---
+++ paymentPage.php
@@ -0,0 +1,99 @@
+<?php
+    session_start();
+    $title = "Payment";
+    include("template/base_header.php");
+?>
+
+<div class="container mainContainer">
+    <!--SCRIPT-->
+    <script type="text/javascript" src="https://april-institute.myhelcim.com/js/version2.js"></script>
+    <script>
+        function showCard() {
+            document.getElementById("helcimResultsCard").hidden = false;
+        }
+    </script>
+
+    <!--FORM-->
+    <form name="helcimForm" id="helcimForm" action="processSchedule.php?eventType=1" method="POST" class="col s12">
+        <!--RESULTS-->
+        <div class="row">
+            <div class="card red" id="helcimResultsCard" hidden>
+                <div class="card-content white-text">
+                    <div class="row">
+                        <div class="col">
+                            <i class="material-icons">error_outline</i>
+                        </div>
+                        
+                        <div class="col" id="helcimResults">
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+
+        <!--SETTINGS--> 
+        <input type="hidden" id="token" value="2dac6006aa6f682bbf2b79"> 
+        <input type="hidden" id="language" value="en"> 
+        <input type="hidden" id="test" value="1">
+
+        <!--PASSING POST INFORMATION-->
+        <input type="hidden" name="scheduled_with" value="<?php echo $_POST["scheduled_with"]; ?>">
+        <input type="hidden" name="date" value="<?php echo $_POST["date"]; ?>">
+        <input type="hidden" name="time" value="<?php echo $_POST["time"]; ?>">
+        <input type="hidden" name="info" value="<?php echo $_POST["info"]; ?>">
+
+        <!--CARD-INFORMATION--> 
+        <div class="row">
+            <div class="input-field col s8 offset-s2">
+                <input type="text" id="cardNumber" value="">
+                <label for="cardNumber">Credit Card Number</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s2 offset-s2">
+                <input type="text" id="cardExpiryMonth" value="">
+                <label for="cardExpiryMonth" value="">Expiry Month</label>
+            </div>
+            <div class="input-field col s4">
+                <input type="text" id="cardExpiryYear" value="">
+                <label for="cardExpiryYear" value="">Expiry Year</label>
+            </div>
+            <div class="input-field col s2">
+                <input type="text" id="cardCVV" value="">
+                <label for="cardCVV">CVV</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field col s8 offset-s2">
+                <input type="text" id="cardHolderName" value="">
+                <label for="cardHolderName">Full Name</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field col s6 offset-s2">
+                <input type="text" id="cardHolderAddress" value="">
+                <label for="cardHolderAddress">Address</label>
+            </div>
+            <div class="input-field col s2">
+                <input type="text" id="cardHolderPostalCode" value="">
+                <label for="cardHolderPostalCode">Postal Code</label>
+            </div>
+        </div>
+
+        <div class="row">
+            <input type="text" id="amount" value="100.00" disabled><br />
+        </div>
+
+        <!--BUTTON--> 
+        <div class="row">
+            <input class="btn-large waves-effect waves-dark white" type="button" id="buttonProcess" value="Process" onclick="javascript:helcimProcess(); showCard();">
+        </div>
+        
+    </form>
+</div>
+
+<?php
+    include("template/base_footer.php")
+?>
