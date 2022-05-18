721242354224a01bc278ad86ad49c3159d698227
Ryan Kim <ryansci123@gmail.com>
2022-05-18 11:36:49 -0400

    Adds 'display all users' button to editCredits.php
    Adds a 'DISPLAY ALL USERS" button to the Credit Manageement page, which lives on editCredits.php. Queries using an empty string, which should always work.
    
    Additionally, fixed a bug where the return search query from process_editCredits.php was not encoding the searchQuery, which resulted in the page not loading properly on empty query searches.

--- process_editCredits.php
+++ process_editCredits.php
@@ -1,6 +1,7 @@
 <?php
 $uid = htmlentities($_REQUEST['uidToUpdate']);
 $newCreditAmt = htmlentities($_REQUEST['creditAmt' . $uid ]);
+$searchQuery = htmlentities($_REQUEST['searchQuery']);
 error_log(print_r($_REQUEST, TRUE));
 
 require("api.php");
@@ -9,4 +10,5 @@ $api -> connect();
 
 $res = $api -> updateUserCredits($uid, $newCreditAmt);
 
-header("Location: editCredits.php?searchQuery=" . htmlentities($_REQUEST['searchQuery']));
+
+header("Location: editCredits.php?searchQuery=" . urlencode(htmlentities($_REQUEST['searchQuery'])));
